a434a4f00d910724bb7fa02167bfd88659e5de53
troolytrap <kevinmcdermott@uchicago.edu>
2025-03-14 13:50:47 +0000

    pages

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -2,4 +2,13 @@ class DiceController < ApplicationController
   def two_six
     render({ :template => "dice_templates/2d6" })
   end
+  def two_ten
+    render({ :template => "dice_templates/2d10" })
+  end
+  def one_twenty
+    render({ :template => "dice_templates/1d20" })
+  end
+  def five_four
+    render({ :template => "dice_templates/5d4" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # get "/your_first_screen" => "pages#first"
   
   get("dice/2/6", { :controller => "dice", :action => "two_six" })
+  get("dice/2/10", { :controller => "dice", :action => "two_ten" })
+  get("dice/1/20", { :controller => "dice", :action => "one_twenty" })
+  get("dice/5/4", { :controller => "dice", :action => "five_four" })
 end
